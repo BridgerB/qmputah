59b8b79f3fa59e2c3a2bdb7ca112757d14d6facc
BridgerB <git@bridgerb.com>
2022-12-05 22:03:06 -0700

    better format

--- functions/rust/src/main.rs
+++ functions/rust/src/main.rs
@@ -1,15 +1,19 @@
 use scraper::{Html, Selector};
 fn main() {
-    let response = reqwest::blocking::get("https://cannabisprescribersutah.com/prices/")
-        .expect("Could not load url.");
+    let url: &str = "https://cannabisprescribersutah.com/prices/";
+    let price_selector: &str = "#\\36 db5b7a2-9ea2-4ccd-bcaa-b95fe6a5f0e7 > div > section > div > div.x-el.x-el-div.c1-1.c1-2.c1-19.c1-8w.c1-8x.c1-8y.c1-2a.c1-8z.c1-90.c1-91.c1-b.c1-c.c1-92.c1-93.c1-94.c1-95.c1-d.c1-e.c1-f.c1-g > div > div > div:nth-child(1) > div > div > div.x-el.x-el-div.c1-1.c1-2.c1-9l.c1-9m.c1-9n.c1-b.c1-c.c1-d.c1-e.c1-f.c1-g > div:nth-child(3) > div > div > div.x-el.x-el-div.c1-1.c1-2.c1-19.c1-1a.c1-4.c1-77.c1-6h.c1-b.c1-c.c1-9r.c1-d.c1-e.c1-f.c1-g > div";
+    let substring = 1..4;
+    let response = reqwest::blocking::get(url).expect("Could not load url.");
     let document = Html::parse_document(&response.text().unwrap());
-    let selector = Selector::parse("#\\36 db5b7a2-9ea2-4ccd-bcaa-b95fe6a5f0e7 > div > section > div > div.x-el.x-el-div.c1-1.c1-2.c1-19.c1-8w.c1-8x.c1-8y.c1-2a.c1-8z.c1-90.c1-91.c1-b.c1-c.c1-92.c1-93.c1-94.c1-95.c1-d.c1-e.c1-f.c1-g > div > div > div:nth-child(1) > div > div > div.x-el.x-el-div.c1-1.c1-2.c1-9l.c1-9m.c1-9n.c1-b.c1-c.c1-d.c1-e.c1-f.c1-g > div:nth-child(3) > div > div > div.x-el.x-el-div.c1-1.c1-2.c1-19.c1-1a.c1-4.c1-77.c1-6h.c1-b.c1-c.c1-9r.c1-d.c1-e.c1-f.c1-g > div").unwrap();
+    let selector = Selector::parse(price_selector).unwrap();
     let first_element = document.select(&selector).next();
     let price = first_element
         .expect("No element found")
         .text()
-        .collect::<String>()[1..4]
+        .collect::<String>()[substring]
         .parse::<u16>()
         .unwrap();
     println!("{}", price);
+
+
 }
